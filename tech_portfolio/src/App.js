d3939eb05a055fc7d19cdea8b31c2aff2dd5b199
Kody <kodyhatcher@gmail.com>
2025-06-10 15:56:51 -0500

    changed resume button

--- tech_portfolio/src/App.js
+++ tech_portfolio/src/App.js
@@ -114,7 +114,7 @@ function App() {
           href="/KodyHatcherResumeMain.pdf"
           target="_blank"
           rel="noopener noreferrer"
-          className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-green-700 transition"
+          className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition"
         >
           Resume
         </a>
